c6c2e62433d12018f8e9b3dcdd6fe484dfa2e9ec
Ariel Ropek <ariel.ropek@panther.com>
2024-05-22 15:07:23 -0600

    black formatting

--- panther_analysis/rules/asana_rules/asana_workspace_org_export.py
+++ panther_analysis/rules/asana_rules/asana_workspace_org_export.py
@@ -66,8 +66,7 @@ class AsanaWorkspaceOrgExport(AsanaRule):
 
     def title(self, event):
         actor_email = event.deep_get("actor", "email", default="<ACTOR_NOT_FOUND>")
-        context_type = event.deep_get("context", "context_type", default="<CONTEXT_TYPE_NOT_FOUND>"
-        )
+        context_type = event.deep_get("context", "context_type", default="<CONTEXT_TYPE_NOT_FOUND>")
         return (
             f"Asana user [{actor_email}] started a [{context_type}] export for your organization."
         )
