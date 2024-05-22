77b3907db27ae064925b7aa67859c7a939e51e7a
Ariel Ropek <ariel.ropek@panther.com>
2024-05-22 17:30:42 -0600

    better RuleID and vars

--- panther_analysis/rules/auth0_rules/auth0_base.py
+++ panther_analysis/rules/auth0_rules/auth0_base.py
@@ -2,18 +2,18 @@ from panther_analysis.base import PantherRule, Severity
 
 
 class Auth0Rule(PantherRule):
-    RuleID = "Auth0.Base-prototype"
+    RuleID = "Auth0Rule"
     Severity = Severity.Info
     Enabled = True
     LogTypes = ["Auth0.Events"]
 
     def alert_context(self, event):
-        a_c = {}
-        a_c["actor"] = event.deep_get(
+        alert_context = {}
+        alert_context["actor"] = event.deep_get(
             "data", "details", "request", "auth", "user", default="<NO_ACTOR_FOUND>"
         )
-        a_c["action"] = event.deep_get("data", "description", default="<NO_ACTION_FOUND>")
-        return a_c
+        alert_context["action"] = event.deep_get("data", "description", default="<NO_ACTION_FOUND>")
+        return alert_context
 
     def is_auth0_config_event(self, event):
         channel = event.deep_get("data", "details", "request", "channel", default="")
