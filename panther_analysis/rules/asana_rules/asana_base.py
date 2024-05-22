5dace14581e7f58608fa2e1279d04a86ccafb858
Ariel Ropek <ariel.ropek@panther.com>
2024-05-22 17:16:38 -0600

    better var names in alert_context

--- panther_analysis/rules/asana_rules/asana_base.py
+++ panther_analysis/rules/asana_rules/asana_base.py
@@ -8,7 +8,7 @@ class AsanaRule(PantherRule):
     Severity = Severity.Info
 
     def alert_context(self, event):
-        a_c = {
+        alert_context = {
             "actor": "<NO_ACTOR>",
             "context": "<NO_CONTEXT>",
             "event_type": "<NO_EVENT_TYPE>",
@@ -17,22 +17,26 @@ class AsanaRule(PantherRule):
             "resource_gid": "<NO_RESOURCE_GID>",
         }
         if event.deep_get("actor", "actor_type", default="") == "user":
-            a_c["actor"] = event.deep_get("actor", "email", default="<NO_ACTOR_EMAIL>")
+            alert_context["actor"] = event.deep_get("actor", "email", default="<NO_ACTOR_EMAIL>")
         else:
-            a_c["actor"] = event.deep_get("actor", "actor_type", default="<NO_ACTOR>")
+            alert_context["actor"] = event.deep_get("actor", "actor_type", default="<NO_ACTOR>")
         if "event_type" in event:
             # Events have categories and event_type
             # We have not seen category overlap -> only including event_type
-            a_c["event_type"] = event.get("event_type")
-        a_c["resource_name"] = event.deep_get("resource", "name", default="<NO_RESOURCE_NAME>")
-        a_c["resource_gid"] = event.deep_get("resource", "gid", default="<NO_RESOURCE_GID>")
-        r_t = event.deep_get("resource", "resource_type")
-        if r_t:
-            a_c["resource_type"] = r_t
-        r_s_t = event.deep_get("resource", "resource_subtype")
-        if r_t and r_s_t and r_s_t != r_t:
-            a_c["resource_type"] += "__" + r_s_t
-        ctx = event.deep_get("context", "context_type")
-        if ctx:
-            a_c["context"] = ctx
-        return a_c
+            alert_context["event_type"] = event.get("event_type")
+        alert_context["resource_name"] = event.deep_get(
+            "resource", "name", default="<NO_RESOURCE_NAME>"
+        )
+        alert_context["resource_gid"] = event.deep_get(
+            "resource", "gid", default="<NO_RESOURCE_GID>"
+        )
+        res_type = event.deep_get("resource", "resource_type")
+        if res_type:
+            alert_context["resource_type"] = res_type
+        res_subtype = event.deep_get("resource", "resource_subtype")
+        if res_type and res_subtype and res_subtype != res_type:
+            alert_context["resource_type"] += "__" + res_subtype
+        context = event.deep_get("context", "context_type")
+        if context:
+            alert_context["context"] = context
+        return alert_context
