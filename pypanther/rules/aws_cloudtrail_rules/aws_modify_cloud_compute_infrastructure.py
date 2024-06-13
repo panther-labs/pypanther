06cdf92dcc92cfad024248132b8e115c80bdc0fe
Ariel Ropek <ariel.ropek@panther.com>
2024-06-13 12:05:09 -0600

    fixed additional aux func errors

--- pypanther/rules/aws_cloudtrail_rules/aws_modify_cloud_compute_infrastructure.py
+++ pypanther/rules/aws_cloudtrail_rules/aws_modify_cloud_compute_infrastructure.py
@@ -538,11 +538,15 @@ class AWSModifyCloudComputeInfrastructure(PantherRule):
         return False
 
     def title(self, event):
-        items = deep_get(event, "requestParameters", "instancesSet", "items", default=[])
+        items = deep_get(
+            event, "requestParameters", "instancesSet", "items", default=[{"instanceId": "none"}]
+        )
         return f"AWS Event [{event.get('eventName')}] Instance ID [{items[0].get('instanceId')}] AWS Account ID [{event.get('recipientAccountId')}]"
 
     def alert_context(self, event):
-        items = deep_get(event, "requestParameters", "instancesSet", "items")
+        items = deep_get(
+            event, "requestParameters", "instancesSet", "items", default=[{"instanceId": "none"}]
+        )
         return {
             "awsRegion": event.get("awsRegion"),
             "eventName": event.get("eventName"),
