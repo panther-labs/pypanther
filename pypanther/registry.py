0b2c33e8483505f10a82c5c4258e7172f56db086
Kostas Papageorgiou <kostas.papageorgiou@panther.com>
2024-07-17 15:37:15 +0300

    Reverting changes

--- pypanther/registry.py
+++ pypanther/registry.py
@@ -9,15 +9,15 @@ __DATA_MODEL_REGISTRY: Set[Type[DataModel]] = set()
 
 def register(arg: Type[Rule] | Iterable[Type[Rule]] | Type[DataModel] | Iterable[Type[DataModel]]):
     """The register function is used to register rules and data models with the pypanther library."""
-    if isinstance(arg, type) and issubclass(arg, Rule):
+    if _is_rule(arg):
         register_rule(arg)
         return
-    if isinstance(arg, type) and issubclass(arg, DataModel):
+    if _is_data_model(arg):
         __DATA_MODEL_REGISTRY.add(arg)
         return
 
     for e in iter(arg):
-        if isinstance(e, type) and issubclass(e, Rule):
+        if _is_rule(e):
             register_rule(e)
         elif isinstance(e, type) and issubclass(e, DataModel):
             __DATA_MODEL_REGISTRY.add(e)
@@ -26,7 +26,7 @@ def register(arg: Type[Rule] | Iterable[Type[Rule]] | Type[DataModel] | Iterable
 
 
 def register_rule(rule: Type[Rule]):
-    if not isinstance(rule, type) or not issubclass(rule, Rule):
+    if not _is_rule(rule):
         raise ValueError(f"rule must be a Rule subclass not {rule}")
 
     rule.validate()
@@ -39,3 +39,9 @@ def registered_rules() -> Set[Type[Rule]]:
 
 def registered_data_models() -> Set[Type[DataModel]]:
     return __DATA_MODEL_REGISTRY
+
+def _is_rule(rule: Type[Rule]) -> bool:
+    return isinstance(rule, type) and issubclass(rule, Rule)
+
+def _is_data_model(data_model: Type[DataModel]) -> bool:
+    return isinstance(data_model, type) and issubclass(data_model, DataModel)
