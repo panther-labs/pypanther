b214088441a27b2fb1b52da067f080c948c3643e
Kostas Papageorgiou <kostas.papageorgiou@panther.com>
2024-07-17 15:06:13 +0300

    Added datamodel registry

--- pypanther/registry.py
+++ pypanther/registry.py
@@ -1,31 +1,50 @@
+import copy
 from typing import Iterable, Set, Type
 
 from pypanther.base import Rule
+from pypanther.data_models_v2 import DataModel
 
-__REGISTRY: Set[Type[Rule]] = set()
+__RULE_REGISTRY: Set[Type[Rule]] = set()
+__DATA_MODEL_REGISTRY: Set[Type[DataModel]] = set()
 
 
-def register(rule: Type[Rule] | Iterable[Type[Rule]]):
-    if isinstance(rule, type) and issubclass(rule, Rule):
-        register_rule(rule)
+def register(arg: Type[Rule] | Iterable[Type[Rule]] | Type[DataModel] | Iterable[Type[DataModel]]):
+    """The register function is used to register rules and data models with the pypanther library."""
+    if _is_rule(arg):
+        register_rule(arg)
         return
-    try:
-        for r in iter(rule):
-            register_rule(r)
+    if _is_data_model(arg):
+        __DATA_MODEL_REGISTRY.add(arg)
         return
-    except TypeError:
-        pass
 
-    raise ValueError(f"rule must be a Rule or an iterable of Rule not {rule}")
+    for e in iter(arg):
+        if _is_rule(e):
+            register_rule(e)
+        elif _is_data_model(e):
+            __DATA_MODEL_REGISTRY.add(e)
+        else:
+            ValueError(f"argument must be a Rule or DataModel or an iterable them not {arg}")
 
 
 def register_rule(rule: Type[Rule]):
-    if not isinstance(rule, type) and issubclass(rule, Rule):
+    if not _is_rule(rule):
         raise ValueError(f"rule must be a Rule subclass not {rule}")
 
     rule.validate()
-    __REGISTRY.add(rule)
+    __RULE_REGISTRY.add(rule)
 
 
 def registered_rules() -> Set[Type[Rule]]:
-    return __REGISTRY
+    return __RULE_REGISTRY
+
+
+def registered_data_models() -> Set[Type[DataModel]]:
+    return __DATA_MODEL_REGISTRY
+
+
+def _is_rule(rule: Type[Rule]) -> bool:
+    return isinstance(rule, type) and issubclass(rule, Rule)
+
+
+def _is_data_model(dm: Type[DataModel]) -> bool:
+    return isinstance(dm, type) and issubclass(dm, DataModel)
--- tests/test_registry.py
+++ tests/test_registry.py
@@ -1,9 +1,10 @@
 from pypanther import register
 from pypanther.base import Rule, Severity
-from pypanther.registry import registered_rules
+from pypanther.data_models_v2 import DataModel
+from pypanther.registry import registered_rules, registered_data_models
 
 
-def test_register_duplicate():
+def test_register_rule_duplicate():
     class A(Rule):
         tags = ["test"]
         log_types = [""]
@@ -17,3 +18,56 @@ def test_register_duplicate():
     A.tags.append("test2")
     register(A)
     assert len(registered_rules()) == 1
+
+
+def test_register_rules():
+    class A(Rule):
+        log_types = [""]
+        id = "a"
+        default_severity = Severity.INFO
+
+        def rule(self, _):
+            pass
+
+    class B(Rule):
+        log_types = [""]
+        id = "b"
+        default_severity = Severity.INFO
+
+        def rule(self, _):
+            pass
+
+    register([A, B])
+    assert len(registered_rules()) == 2
+    assert A in registered_rules()
+    assert B in registered_rules()
+
+
+def test_register_data_model_duplicate():
+    class A(DataModel):
+        data_model_id = "test_register_duplicate"
+
+    register(A)
+    register(A)
+    assert len(registered_data_models()) == 1
+
+
+def test_register_data_models():
+    class A(DataModel):
+        id = "a"
+
+    class B(DataModel):
+        id = "b"
+
+    register([A, B])
+    assert len(registered_data_models()) == 2
+    assert A in registered_data_models()
+    assert B in registered_data_models()
+
+
+def test_invalid_argument():
+    register("hello")
+
+
+def test_invalid_argument_in_iterable():
+    register(["hello"])
