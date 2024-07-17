834d687683631b95575520013ba4d1d9cb1fd9de
Kostas Papageorgiou <kostas.papageorgiou@panther.com>
2024-07-17 16:01:07 +0300

    Removing some linter checks

--- pypanther/registry.py
+++ pypanther/registry.py
@@ -9,17 +9,17 @@ __DATA_MODEL_REGISTRY: Set[Type[DataModel]] = set()
 
 def register(arg: Type[Rule] | Iterable[Type[Rule]] | Type[DataModel] | Iterable[Type[DataModel]]):
     """The register function is used to register rules and data models with the pypanther library."""
-    if _is_rule(arg):  # type: ignore[arg-type]
+    if _is_rule(arg):
         register_rule(arg)  # type: ignore[arg-type]
         return
-    if _is_data_model(arg):  # type: ignore[arg-type]
+    if _is_data_model(arg):
         __DATA_MODEL_REGISTRY.add(arg)  # type: ignore[arg-type]
         return
 
     for e in iter(arg):  # type: ignore[arg-type]
-        if _is_rule(e):  # type: ignore[arg-type]
+        if _is_rule(e):
             register_rule(e)  # type: ignore[arg-type]
-        elif _is_data_model(e):  # type: ignore[arg-type]
+        elif _is_data_model(e):
             __DATA_MODEL_REGISTRY.add(e)  # type: ignore[arg-type]
         else:
             ValueError(f"argument must be a Rule or DataModel or an iterable them not {arg}")
@@ -41,9 +41,9 @@ def registered_data_models() -> Set[Type[DataModel]]:
     return __DATA_MODEL_REGISTRY
 
 
-def _is_rule(rule: Type[Rule]) -> bool:
+def _is_rule(rule) -> bool:
     return isinstance(rule, type) and issubclass(rule, Rule)
 
 
-def _is_data_model(data_model: Type[DataModel]) -> bool:
-    return isinstance(data_model, type) and issubclass(data_model, DataModel)
+def _is_data_model(dm) -> bool:
+    return isinstance(dm, type) and issubclass(dm, DataModel)
