26248c4136b5d9aa0dceaa939961ab784c76003c
Kostas Papageorgiou <kostas.papageorgiou@panther.com>
2024-07-23 17:20:46 +0300

    Experimental

--- pypanther/data_models_v2.py
+++ pypanther/data_models_v2.py
@@ -1,8 +1,11 @@
-import abc
+from abc import ABC
 import copy
-import dataclasses
+from dataclasses import dataclass
+import datetime
 from enum import Enum
-from typing import List, Optional
+from typing import List, Optional, final
+
+from pydantic import BaseModel
 
 from pypanther import LogType
 from pypanther.utils import try_asdict
@@ -33,7 +36,7 @@ _FIELD_MAPPING_ALL_ATTRIBUTES = [
 ]
 
 
-@dataclasses.dataclass
+@dataclass
 class FieldMapping:
     """Represents a field mapping in a data model."""
 
@@ -52,93 +55,119 @@ class FieldMapping:
 
 
 _FIELD_ALL_ATTRIBUTES = [
-    "name",
     "type",
     "mappings",
-    "description",
 ]
 
 
-@dataclasses.dataclass
-class Field:
+@dataclass
+class Field(ABC):
     """Represents a field in a data model."""
-
-    name: str
-    """The name of the field. This is the key that will be used to access the field in the data model."""
-
     type: FieldType
     """The type of the field."""
 
-    mappings: List[FieldMapping]
+    mappings: list[FieldMapping]
     """Mappings describe how the data model field is derived from the various log types."""
 
-    description: str = ""
-    """A description of the field."""
 
-    def asdict(self):
-        """Returns a dictionary representation of the instance."""
-        return {key: try_asdict(getattr(self, key)) for key in _FIELD_ALL_ATTRIBUTES if hasattr(self, key)}
-
-
-_DATA_MODEL_ALL_ATTRS = [
-    "id",
-    "description",
-    "enabled",
-    "fields",
-]
+@final
+@dataclass
+class String(str, Field):
+    """ Represents a string """
+    pass
 
 
-class DataModel(abc.ABC):
+def new_string(mappings: list[FieldMapping]) -> String:
+    return String(type=FieldType.STRING, mappings=mappings)
+
+
+@final
+@dataclass
+class SmallInt(Field, int):
+    """ Represents a 32-bit signed integer """
+    pass
+
+
+def new_smallint(mappings: list[FieldMapping]) -> SmallInt:
+    return SmallInt(type=FieldType.SMALL_INT, mappings=mappings)
+
+
+@final
+@dataclass
+class BigInt(int, Field):
+    """ Represents a 64-bit signed integer """
+    pass
+
+
+def new_bigint(mappings: list[FieldMapping]) -> BigInt:
+    return BigInt(type=FieldType.BIG_INT, mappings=mappings)
+
+
+@final
+@dataclass
+class Float(float, Field):
+    """ Represents a floating point number """
+    pass
+
+
+def new_float(mappings: list[FieldMapping]) -> Float:
+    return Float(type=FieldType.FLOAT, mappings=mappings)
+
+
+@final
+@dataclass
+class Bool(bool, Field):
+    """ Represents a boolean value """
+    pass
+
+
+def new_bool(mappings: list[FieldMapping]) -> Bool:
+    return Bool(type=FieldType.BOOLEAN, mappings=mappings)
+
+
+@final
+@dataclass
+class Timestamp(datetime.datetime, Field):
+    """ Represents a timestamp """
+    pass
+
+
+def new_timestamp(mappings: list[FieldMapping]) -> datetime.datetime:
+    return Timestamp(type=FieldType.TIMESTAMP, mappings=mappings)
+
+
+@final
+@dataclass
+class Array(list, Field):
+    """ Represents an array of items """
+    pass
+
+
+def new_array(mappings: list[FieldMapping]) -> list:
+    return Array(type=FieldType.ARRAY, mappings=mappings)
+
+
+@final
+@dataclass
+class Object(dict[str, any], Field):
+    """ Represents an object"""
+    pass
+
+
+def new_object(mappings: list[FieldMapping]) -> dict[str, any]:
+    return Object(type=FieldType.OBJECT, mappings=mappings)
+
+
+@final
+@dataclass
+class JSON(any, Field):
+    """ Represents a JSON object """
+    pass
+
+
+def new_json(mappings: list[FieldMapping]) -> any:
+    return JSON(type=FieldType.JSON, mappings=mappings)
+
+
+class DataModel(BaseModel):
     """A Panther data model. This class should be subclassed to create a new Data Model."""
-
-    id: str
-    """The unique identifier of the data model."""
-
-    description: str = ""
-    """A description of the data model."""
-
-    enabled: bool = True
-    """Whether the data model is enabled can can be used."""
-
-    fields: List[Field]
-    """The fields that make up the data model."""
-
-    @classmethod
-    def is_panther_managed(cls) -> bool:
-        return cls.__module__.startswith("pypanther")
-
-    @classmethod
-    def override(
-        cls,
-        id: Optional[str] = None,
-        description: Optional[str] = None,
-        enabled: Optional[bool] = None,
-        fields: Optional[List[Field]] = None,
-    ):
-        for key, val in locals().items():
-            if key == "cls":
-                continue
-
-            if val is not None:
-                setattr(cls, key, val)
-
-    def __init_subclass__(cls, **kwargs):
-        """Creates a copy of all class attributes to avoid modifications affecting parent.fields.
-        child.fields.append(Field("foo",...))
-        parent.fields.append(Field("foo",...) # not inherited by children of parent
-        """
-        for attr in _DATA_MODEL_ALL_ATTRS:
-            if attr not in cls.__dict__:
-                try:
-                    v = getattr(cls, attr)
-                except AttributeError:
-                    v = None
-
-                if v is not None:
-                    setattr(cls, attr, copy.deepcopy(v))
-        super().__init_subclass__(**kwargs)
-
-    @classmethod
-    def asdict(cls):
-        """Returns a dictionary representation of the class."""
-        return {key: try_asdict(getattr(cls, key)) for key in _DATA_MODEL_ALL_ATTRS if hasattr(cls, key)}
