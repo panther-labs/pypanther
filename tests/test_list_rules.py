399761dddf105ae9fdb61c2ccdcf6dc6df824bf2
maxrichie5 <maxrichmond@panther.com>
2024-08-14 13:45:03 -0600

    updates

--- tests/test_list_rules.py
+++ tests/test_list_rules.py
@@ -32,12 +32,12 @@ FILTER_ARGS = [
 ]
 
 
-def test_list_no_mangaed() -> None:
+def test_list_no_managed_or_registered() -> None:
     args = setup_parser().parse_args(f"{LIST_RULES_CMD}".split(" "))
     assert not args.managed and not args.registered
     code, err = list_rules.run(args)
-    assert code == 1
-    assert str(err) == "At least one of --registered or --managed is required"
+    assert code == 0
+    assert err == ""
 
 
 def test_list_registered_no_main() -> None:
