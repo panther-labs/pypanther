4e3417e93a0989abfd0574ecad1f2437b350e4ff
Ben Airey <benjaminjohnairey@gmail.com>
2024-09-18 16:20:25 -0500

    added severity upgrade/downgrade, and tests

--- tests/test_severity.py
+++ tests/test_severity.py
@@ -1,3 +1,5 @@
+import pytest
+
 from pypanther.severity import Severity
 
 
@@ -14,3 +16,23 @@ def test_severity_as_int():
     assert Severity.as_int(Severity.MEDIUM) == 2
     assert Severity.as_int(Severity.HIGH) == 3
     assert Severity.as_int(Severity.CRITICAL) == 4
+
+@pytest.mark.parametrize(("sev1", "sev2"), [
+    ("CRITICAL", "HIGH"),
+    ("HIGH", "MEDIUM"),
+    ("MEDIUM", "LOW"),
+    ("LOW", "INFO"),
+    ("INFO", "INFO")
+])
+def test_severity_downgrading(sev1, sev2):
+    assert Severity(sev1).downgrade() == Severity(sev2)
+
+@pytest.mark.parametrize(("sev1", "sev2"), [
+    ("CRITICAL", "CRITICAL"),
+    ("HIGH", "CRITICAL"),
+    ("MEDIUM", "HIGH"),
+    ("LOW", "MEDIUM"),
+    ("INFO", "LOW")
+])
+def test_severity_upgrading(sev1, sev2):
+    assert Severity(sev1).upgrade() == Severity(sev2)
