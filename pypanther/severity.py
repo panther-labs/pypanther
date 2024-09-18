e68bca996210dfdc7fd9d90d1aefce2a4856f688
Ben Airey <benjaminjohnairey@gmail.com>
2024-09-18 17:01:25 -0500

    made linter happy

--- pypanther/severity.py
+++ pypanther/severity.py
@@ -14,27 +14,31 @@ class Severity(str, Enum):
 
     def __lt__(self, other):
         return Severity.as_int(self.value) < Severity.as_int(other.value)
-    
+
     def _increment(self, amt: int = 1) -> Severity:
         """Increase or decrease the severity level by a given amount."""
         levels = [Severity.INFO, Severity.LOW, Severity.MEDIUM, Severity.HIGH, Severity.CRITICAL]
         current_idx = levels.index(self)
         # Change index by ammount, clamping between 0 and 4
-        new_idx = min(max(0, current_idx + amt), len(levels)-1)
+        new_idx = min(max(0, current_idx + amt), len(levels) - 1)
         return levels[new_idx]
-    
+
     def upgrade(self):
-        """Increase the severity level by 1. i.e. MEDIUM -> HIGH.
-        Cannot return higher than CRITICAL."""
+        """
+        Increase the severity level by 1. i.e. MEDIUM -> HIGH.
+        Cannot return higher than CRITICAL.
+        """
         return self._increment(1)
-    
+
     def downgrade(self):
-        """Decrease the severity level by 1. i.e. MEDIUM -> LOW.
-        Cannot return lower than INFO."""
+        """
+        Decrease the severity level by 1. i.e. MEDIUM -> LOW.
+        Cannot return lower than INFO.
+        """
         return self._increment(-1)
 
     @staticmethod
-    def as_int(value: "Severity") -> int:
+    def as_int(value: Severity) -> int:
         val = value.upper()
         if val == Severity.INFO:
             return 0
