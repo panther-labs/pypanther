81e6970adb493c9227286d0bdb2df064e8932a20
Rohan Tandon <rohan.tandon@panther.com>
2024-09-24 15:44:40 -0700

    Create exclusion groups

--- pypanther/shared_args.py
+++ pypanther/shared_args.py
@@ -16,13 +16,17 @@ def for_filtering(parser: argparse.ArgumentParser) -> None:
         default=None,
         required=False,
     )
-    parser.add_argument(
+
+    create_alert_group = parser.add_mutually_exclusive_group()
+
+    create_alert_group.add_argument(
         "--create-alert",
         help="Filter by items that create alerts",
         default=None,
         action="store_true",
     )
-    parser.add_argument(
+
+    create_alert_group.add_argument(
         "--no-create-alert",
         help="Filter by items that don't create alerts",
         dest="create_alert",
@@ -42,13 +46,15 @@ def for_filtering(parser: argparse.ArgumentParser) -> None:
         default=None,
         required=False,
     )
-    parser.add_argument(
+    enable_disable_group = parser.add_mutually_exclusive_group()
+
+    enable_disable_group.add_argument(
         "--enabled",
         help="Filter only on enabled items",
         default=None,
         action="store_true",
     )
-    parser.add_argument(
+    enable_disable_group.add_argument(
         "--disabled",
         help="Filter only on disabled items",
         dest="enabled",
