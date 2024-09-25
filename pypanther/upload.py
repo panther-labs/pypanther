6afe44d7c23739a4d086e67b9da8624667589490
Kostas Papageorgiou <kostas.papageorgiou@panther.com>
2024-09-25 14:54:16 +0300

    fixing imports

--- pypanther/upload.py
+++ pypanther/upload.py
@@ -13,7 +13,7 @@ from typing import Any, Optional, Tuple
 from pypanther import cli_output, display, testing
 from pypanther.import_main import NoMainModuleError, import_main
 from pypanther.registry import registered_rules
-from pypanther.vendor.panther_analysis_tool.backend.client import (
+from pypanther.backend.client import (
     AsyncBulkUploadParams,
     AsyncBulkUploadStatusParams,
     AsyncBulkUploadStatusResponse,
@@ -21,8 +21,8 @@ from pypanther.vendor.panther_analysis_tool.backend.client import (
     BackendResponse,
     BulkUploadMultipartError,
 )
-from pypanther.vendor.panther_analysis_tool.backend.client import Client as BackendClient
-from pypanther.vendor.panther_analysis_tool.util import convert_unicode
+from pypanther.backend.client import Client as BackendClient
+from pypanther.backend.util import convert_unicode
 
 INDENT = " " * 2
 IGNORE_FOLDERS = [
