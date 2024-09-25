9ad5d21422437ebd90c00ae5b61726a66cedf7ed
Kostas Papageorgiou <kostas.papageorgiou@panther.com>
2024-09-25 14:51:23 +0300

    Removing vendor folder

--- pypanther/backend/util.py
+++ pypanther/backend/util.py
@@ -22,9 +22,9 @@ import re
 from typing import Any, Callable, Tuple
 
 from pypanther import display
-from pypanther.vendor.panther_analysis_tool.backend.client import Client as BackendClient
-from pypanther.vendor.panther_analysis_tool.backend.public_api_client import PublicAPIClient, PublicAPIClientOptions
-from pypanther.vendor.panther_analysis_tool.constants import PANTHER_USER_ID
+from pypanther.backend.client import Client as BackendClient
+from pypanther.backend.public_api_client import PublicAPIClient, PublicAPIClientOptions
+from pypanther.constants import PANTHER_USER_ID
 
 UNKNOWN_VERSION = "unknown"
 
