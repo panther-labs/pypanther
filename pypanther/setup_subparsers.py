7a5b2db55a58df06ec9c8f4f8074489946ecb94e
Nikos Vlastaras <nikolaos.vlastaras@ext.runpanther.io>
2024-10-01 15:30:09 +0300

    Refactor to mutually exclusive group

--- pypanther/setup_subparsers.py
+++ pypanther/setup_subparsers.py
@@ -117,14 +117,15 @@ def setup_upload_parser(upload_parser: argparse.ArgumentParser):
         choices=display.COMMON_CLI_OUTPUT_TYPES,
         default=display.DEFAULT_CLI_OUTPUT_TYPE,
     )
-    upload_parser.add_argument(
+    dry_run_group = upload_parser.add_mutually_exclusive_group()
+    dry_run_group.add_argument(
         "--skip-summary",
         help="Omit changes summary in output",
         default=False,
         required=False,
         action="store_true",
     )
-    upload_parser.add_argument(
+    dry_run_group.add_argument(
         "--dry-run",
         help="Avoid actually uploading",
         default=False,
