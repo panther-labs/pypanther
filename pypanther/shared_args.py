f4949390aff3e6cc6f0d21bd0045f7c0cc942af1
Kostas Papageorgiou <kostas.papageorgiou@panther.com>
2024-10-22 14:47:58 +0300

    Fixing linter issues

--- pypanther/shared_args.py
+++ pypanther/shared_args.py
@@ -17,7 +17,6 @@ def for_filtering(parser: argparse.ArgumentParser) -> None:
         required=False,
     )
 
-
     parser.add_argument(
         "--create-alert",
         help="Filter by items that create alerts or don't create alerts",
@@ -105,12 +104,12 @@ def for_filtering(parser: argparse.ArgumentParser) -> None:
         required=False,
     )
 
+
 def str2bool(v):
     if isinstance(v, bool):
         return v
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+    if v.lower() in ("yes", "true", "t", "y", "1"):
         return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+    if v.lower() in ("no", "false", "f", "n", "0"):
         return False
-    else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
+    raise argparse.ArgumentTypeError("Boolean value expected.")
