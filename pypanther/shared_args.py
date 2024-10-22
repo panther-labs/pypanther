682b27c4611e9031b836a9be994819c7fcf11a49
Kostas Papageorgiou <kostas.papageorgiou@panther.com>
2024-10-22 17:23:58 +0300

    Bring them back

--- pypanther/shared_args.py
+++ pypanther/shared_args.py
@@ -108,8 +108,8 @@ def for_filtering(parser: argparse.ArgumentParser) -> None:
 def str2bool(v):
     if isinstance(v, bool):
         return v
-    if v.lower() == "true":
+    if v.lower() in ("yes", "true", "t", "y", "1"):
         return True
-    if v.lower() == "false":
+    if v.lower() in ("no", "false", "f", "n", "0"):
         return False
     raise argparse.ArgumentTypeError("Boolean value expected.")
