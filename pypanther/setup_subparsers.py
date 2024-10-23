adb9136851b73403c030a72e3116b1d40b1fd569
Kostas Papageorgiou <kostas.papageorgiou@panther.com>
2024-10-23 12:18:55 +0300

    Changing flag

--- pypanther/setup_subparsers.py
+++ pypanther/setup_subparsers.py
@@ -3,6 +3,7 @@ import pathlib
 
 from pypanther import display, generate, get_rule, list_log_types, list_rules, shared_args, upload
 from pypanther.backend import util
+from pypanther.shared_args import str2bool
 
 
 def setup_list_rules_parser(list_rules_parser: argparse.ArgumentParser):
@@ -10,10 +11,10 @@ def setup_list_rules_parser(list_rules_parser: argparse.ArgumentParser):
     shared_args.for_filtering(list_rules_parser)
     list_rules_parser.add_argument(
         "--managed",
-        help="List panther managed rules instead of registered rules to view available rules",
-        default=False,
+        help="Filter by Panther managed or non-Panther managed rules",
+        default=None,
         required=False,
-        action="store_true",
+        type=str2bool,
     )
     list_rules_parser.add_argument(
         "--attributes",
